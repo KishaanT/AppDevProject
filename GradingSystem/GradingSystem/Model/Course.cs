c80975ff98226067be1ded31413f451e2f84be95
KishaanT <kishaan2006@gmail.com>
2024-12-01 18:17:21 -0500

    Changed whole Shebang
    YEAAAHHH BUDDY

--- GradingSystem/GradingSystem/Model/Course.cs
+++ GradingSystem/GradingSystem/Model/Course.cs
@@ -12,5 +12,13 @@ namespace GradingSystem
         public string CourseName { get; set; }
         public List<Assignment> Assignments { get; set; } = new List<Assignment>();
         public Dictionary<int, double> StudentAverages { get; set; } = new Dictionary<int, double>();
+
+        //public Course(int courseId, string courseName, List<Assignment> assignments, Dictionary<int, double> studentAverages)
+        //{
+        //    CourseId = courseId;
+        //    CourseName = courseName;
+        //    Assignments = assignments;
+        //    StudentAverages = studentAverages;
+        //}
     }
 }
